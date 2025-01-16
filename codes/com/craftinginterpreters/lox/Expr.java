a73b35bbe02687217e44dcbefc17fcd5c33028c0
cimi21036 <cimi21036@g.nihon-u.ac.jp>
2025-01-16 11:52:29 +0900

    add Stmt,Enviroment,RuntimeError/modified Expr

--- codes/com/craftinginterpreters/lox/Expr.java
+++ codes/com/craftinginterpreters/lox/Expr.java
@@ -19,7 +19,8 @@ abstract class Expr {
     R visitVariableExpr(Variable expr);
   }
 
- 
+  // Nested Expr classes here...
+//> expr-assign
   static class Assign extends Expr {
     Assign(Token name, Expr value) {
       this.name = name;
@@ -34,7 +35,8 @@ abstract class Expr {
     final Token name;
     final Expr value;
   }
-
+//< expr-assign
+//> expr-binary
   static class Binary extends Expr {
     Binary(Expr left, Token operator, Expr right) {
       this.left = left;
@@ -51,7 +53,8 @@ abstract class Expr {
     final Token operator;
     final Expr right;
   }
-
+//< expr-binary
+//> expr-call
   static class Call extends Expr {
     Call(Expr callee, Token paren, List<Expr> arguments) {
       this.callee = callee;
@@ -68,7 +71,8 @@ abstract class Expr {
     final Token paren;
     final List<Expr> arguments;
   }
-
+//< expr-call
+//> expr-get
   static class Get extends Expr {
     Get(Expr object, Token name) {
       this.object = object;
@@ -83,7 +87,8 @@ abstract class Expr {
     final Expr object;
     final Token name;
   }
-
+//< expr-get
+//> expr-grouping
   static class Grouping extends Expr {
     Grouping(Expr expression) {
       this.expression = expression;
@@ -96,7 +101,8 @@ abstract class Expr {
 
     final Expr expression;
   }
-
+//< expr-grouping
+//> expr-literal
   static class Literal extends Expr {
     Literal(Object value) {
       this.value = value;
@@ -109,7 +115,8 @@ abstract class Expr {
 
     final Object value;
   }
-
+//< expr-literal
+//> expr-logical
   static class Logical extends Expr {
     Logical(Expr left, Token operator, Expr right) {
       this.left = left;
@@ -126,7 +133,8 @@ abstract class Expr {
     final Token operator;
     final Expr right;
   }
-
+//< expr-logical
+//> expr-set
   static class Set extends Expr {
     Set(Expr object, Token name, Expr value) {
       this.object = object;
@@ -143,7 +151,8 @@ abstract class Expr {
     final Token name;
     final Expr value;
   }
-
+//< expr-set
+//> expr-super
   static class Super extends Expr {
     Super(Token keyword, Token method) {
       this.keyword = keyword;
@@ -158,7 +167,8 @@ abstract class Expr {
     final Token keyword;
     final Token method;
   }
-
+//< expr-super
+//> expr-this
   static class This extends Expr {
     This(Token keyword) {
       this.keyword = keyword;
@@ -171,7 +181,8 @@ abstract class Expr {
 
     final Token keyword;
   }
-
+//< expr-this
+//> expr-unary
   static class Unary extends Expr {
     Unary(Token operator, Expr right) {
       this.operator = operator;
@@ -186,7 +197,8 @@ abstract class Expr {
     final Token operator;
     final Expr right;
   }
-
+//< expr-unary
+//> expr-variable
   static class Variable extends Expr {
     Variable(Token name) {
       this.name = name;
@@ -199,7 +211,8 @@ abstract class Expr {
 
     final Token name;
   }
-
+//< expr-variable
 
   abstract <R> R accept(Visitor<R> visitor);
 }
+//< Appendix II expr
